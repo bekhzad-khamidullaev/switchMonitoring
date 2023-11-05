7ba65fc98dc9de25f09acc175dd905834c13f46c
Your Name <you@example.com>
2023-11-05 10:37:59 +0000

    aaa

--- snmp/urls.py
+++ snmp/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = 'snmp'
 
 urlpatterns = [
-    path('vendors/', views.vendor_list, name='vendor_list'),
-    path('device-models/', views.device_model_list, name='device_model_list'),
-    path('olts/', views.olt_list, name='olt_list'),
-    path('switches/', views.switch_list, name='switch_list'),
-    path('olts/<int:olt_id>/', views.olt_detail, name='olt_detail'),
-    path('switches/<int:switch_id>/', views.switch_detail, name='switch_detail'),
-    path('create-olt/', views.create_olt, name='create_olt'),
-    path('create-switch/', views.create_switch, name='create_switch'),
+    # path('vendors/', views.vendor_list, name='vendor_list'),
+    # path('device-models/', views.device_model_list, name='device_model_list'),
+    # path('olts/', views.olt_list, name='olt_list'),
+    # path('switches/', views.switch_list, name='switch_list'),
+    # path('olts/<int:olt_id>/', views.olt_detail, name='olt_detail'),
+    # path('switches/<int:switch_id>/', views.switch_detail, name='switch_detail'),
+    # path('create-olt/', views.create_olt, name='create_olt'),
+    # path('create-switch/', views.create_switch, name='create_switch'),
 ]
