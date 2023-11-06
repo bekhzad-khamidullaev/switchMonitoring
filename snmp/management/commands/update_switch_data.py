4a92de6c89df318625f6f74cc94da7fddcd6e564
Bekhzad Khamidullaev <bekhzad_94@icloud.com>
2023-11-06 13:11:11 +0500

    aaa

--- snmp/management/commands/update_switch_data.py
+++ snmp/management/commands/update_switch_data.py
@@ -3,7 +3,30 @@ from django.core.management.base import BaseCommand
 from snmp.models import Switch
 from snmp.models import Vendor
 from snmp.models import SwitchModel
-from easysnmp import Session
+# Import the necessary SNMP-related modules from pysnmp
+from pysnmp.hlapi import *
+
+def perform_snmpwalk(ip, oid):
+    # Create an SNMP request
+    snmp_walk = getCmd(
+        SnmpEngine(),
+        CommunityData('snmp2netread'),  # Replace 'public' with your community string
+        UdpTransportTarget((ip, 161)),  # Replace '161' with the SNMP port
+        ContextData(),
+        ObjectType(ObjectIdentity(oid)),
+    )
+
+    # Execute the SNMP walk and collect the response
+    snmp_response = []
+    for (errorIndication, errorStatus, errorIndex, varBinds) in snmp_walk:
+        if errorIndication:
+            # Handle SNMP error
+            print(f"SNMP error: {errorIndication}")
+        else:
+            for varBind in varBinds:
+                snmp_response.append(str(varBind))
+
+    return snmp_response
 
 class Command(BaseCommand):
     help = 'Update switch data'
@@ -13,8 +36,11 @@ class Command(BaseCommand):
         ip_addresses = Switch.objects.values_list('device_ip', flat=True)
 
         for ip in ip_addresses:
+            
+            print(ip)
             # Step 3: Check if the host is alive using ping3
             host_alive = ping3.ping(ip)
+            print(host_alive)
 
             if host_alive is not None:
                 # The host is reachable
@@ -34,8 +60,10 @@ class Command(BaseCommand):
                         if device_model:
                             vendor_name = device_model.vendor.name
 
-                    # Step 6: Send an SNMP GET request with the OID '1.3.6.1.2.1.1.1.0' to the selected IP address
-                    snmp_response = perform_snmpget(selected_switch.device_ip, '1.3.6.1.2.1.1.1.0')
+                    
+                    # Step 6: Send an SNMPwalk request with the OID '1.3.6.1.2.1.1.1.0' to the selected IP address
+                    # (You can use an SNMP library like pysnmp to perform SNMP operations)
+                    snmp_response = perform_snmpwalk(selected_switch.device_ip, '1.3.6.1.2.1.1.1.0')
 
                     # Step 7: Check the response for containing the name from the snmp_vendor table
                     vendor_name = selected_switch.device_type.device_model.vendor.name
@@ -48,20 +76,3 @@ class Command(BaseCommand):
                             selected_switch.device_model_local = device_model_name
                             selected_switch.device_hostname = device_model_name
                             selected_switch.save()
-
-def perform_snmpget(device_ip, oid):
-    try:
-        # Create an SNMP session
-        session = Session(hostname=device_ip, community='public', version=2)  # Replace 'public' with your community string
-
-        # Perform an SNMP GET operation
-        response = session.get(oid)
-
-        if response.error:
-            print(f'Error while performing SNMP GET for OID {oid}: {response.error}')
-            return None
-        else:
-            return response.value
-    except Exception as e:
-        print(f'An error occurred while performing SNMP GET: {str(e)}')
-        return None
