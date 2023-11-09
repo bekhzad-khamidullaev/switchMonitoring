868a3a360c0091818800118bc15b28a425b8678a
Your Name <you@example.com>
2023-11-09 07:01:54 +0000

    aaaa

--- snmp/models.py
+++ snmp/models.py
@@ -44,30 +44,26 @@ class Device(models.Model):
 
 
 class Switch(models.Model):
-    device_type = models.ForeignKey(Device, on_delete=models.SET_NULL, null=True)
-    device_model = models.ForeignKey(SwitchModel, on_delete=models.SET_NULL, null=True, blank=True)
     device_model_local = models.CharField(max_length=200, null=True, blank=True)
     device_hostname = models.CharField(max_length=200, null=True, blank=True)
     device_ip = models.GenericIPAddressField(unique=True, protocol='both', null=True, blank=True)
     device_optical_info = models.IntegerField(null=True, blank=True)
     device_snmp_community = models.CharField(max_length=100, default='snmp2netread')
-    device_general_oid = models.CharField(max_length=200, default='1.3.6.1.2.1.1.1.0')
+    sysDescr_oid = models.CharField(max_length=200, default='1.3.6.1.2.1.1.1.0')
     status = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.device_model}"
+        return f"{self.device_hostname}"
 
 
 class Olt(models.Model):
-    device_type = models.ForeignKey(Device, on_delete=models.SET_NULL, null=True)
-    device_model = models.ForeignKey(OltModel, on_delete=models.SET_NULL, null=True, blank=True)
     device_model_local = models.CharField(max_length=200, null=True, blank=True)
     device_hostname = models.CharField(max_length=200, null=True, blank=True)
     device_ip = models.GenericIPAddressField(protocol='both', null=True, blank=True)
     device_optical_info = models.IntegerField(null=True, blank=True)
     device_snmp_community = models.CharField(max_length=100, default='snmp2netread')
-    device_general_oid = models.CharField(max_length=200, default='1.3.6.1.2.1.1.1.0')
+    sysDescr_oid = models.CharField(max_length=200, default='1.3.6.1.2.1.1.1.0')
     status = models.BooleanField(default=False)
         
     def __str__(self):
-        return f"{self.device_model}"
+        return f"{self.device_hostname}"
