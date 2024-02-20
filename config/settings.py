aa68bfc10baa56de52c0d068e28cf096b3a231c1
Bekhzad Khamidullaev <bekzodhamidullayev74@gmail.com>
2024-02-20 20:46:13 +0500

    aaa

--- config/settings.py
+++ config/settings.py
@@ -125,22 +125,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
-
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
-
 CELERY_TIMEZONE = 'Asia/Tashkent'
-
-
 CELERY_IMPORTS = ('snmp.tasks',)
-
-
 CELERY_TRACK_STARTED = True
-
 CELERY_BEAT_SCHEDULE_FILENAME = os.path.join(BASE_DIR, 'celerybeat-schedule.db')
-
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ENABLE_UTC = True
 
 CELERY_BEAT_SCHEDULE = {
     'update-switch-status': {
--- snmp/tasks.py
+++ snmp/tasks.py
@@ -1,27 +1,24 @@
 from celery import shared_task
 from django.core.management import call_command
-from celery import current_task
-from snmp.management.commands.startserver import Command as StartServerCommand
+from snmp.models import Switch
 
-@shared_task
-def runserver_task():
-    start_server_command = StartServerCommand()
-    start_server_command.handle()
 
-@shared_task
-def update_switch_status_task(switch_id):
-    current_task.update_state(state='PROGRESS', meta={'status': 'Updating switch status'})
-    call_command('update_switch_status', switch_id)
-    return {'status': 'Switch status updated successfully'}
     
+# @shared_task
+# def update_switch_status_task(ip):
+#     call_command('update_switch_status', ip)
+
+@shared_task
+def update_switch_status_task(ip):
+    switch = Switch.objects.get(ip=ip)
+    from .management.commands import update_switch_status
+    update_switch_status(ip)
+
 @shared_task
 def update_optical_info_task():
-    current_task.update_state(state='PROGRESS', meta={'status': 'Updating switch optical info'})
     call_command('update_optical_info')
-    return {'status': 'Switch optical info updated successfully'}
-
+    
 @shared_task
 def update_switch_inventory_task():
-    current_task.update_state(state='PROGRESS', meta={'status': 'Updating switch inventory info'})
-    call_command('update_switch_inventory')
-    return {'status': 'Switch inventory info updated successfully'}
+    call_command('update_switch_inventory_task')
+    
