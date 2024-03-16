e4d692c8a4c905a872cc902345a07c60c23f34b5
Bekhzad Khamidullaev <bekzodhamidullayev74@gmail.com>
2024-03-16 21:29:21 +0500

    aaa

--- snmp/management/commands/subnet_discovery.py
+++ snmp/management/commands/subnet_discovery.py
@@ -1,10 +1,8 @@
 import nmap
-import asyncio
 import logging
 from ipaddress import IPv4Network
 from django.core.management.base import BaseCommand
-from django.core.management import call_command
-from snmp.models import Switch, SwitchModel
+from snmp.models import Switch, SwitchModel, Ats
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("SNMP DISCOVERY")
@@ -22,40 +20,33 @@ class Command(BaseCommand):
             logger.error(f"Error while checking host {ip} reachability: {e}")
             return False
 
-    async def handle_subnet(self, subnet, models):
+    def handle_subnet(self, subnet, models):
         hosts = [str(host) for host in list(subnet.hosts())[1:]]
-        tasks = []
         for ip_address in hosts:
             is_reachable = self.check_host_reachability(ip_address)
-
             if is_reachable:
                 switch, created = Switch.objects.get_or_create(ip=ip_address)
-
                 logger.info(f"Processing switch at IP: {ip_address}")
-
-                tasks.append(self.perform_snmp_operations(switch, models))
+                switch.save()
             else:
                 logger.warning(f"Host {ip_address} is not reachable.")
 
-        await asyncio.gather(*tasks)
-
-    async def process_subnets(self):
-        main_subnet = IPv4Network('10.101.0.0/16')
-        subnets = list(main_subnet.subnets(new_prefix=25))
+    def process_subnets(self):
+        ats_subnets = Ats.objects.values_list('subnet', flat=True)
         models = SwitchModel.objects.all()
 
-        tasks = []
-        for subnet in subnets:
-            tasks.append(self.handle_subnet(subnet, models))
-
-        await asyncio.gather(*tasks)
+        for subnet_str in ats_subnets:
+            subnet = IPv4Network(subnet_str)
+            subnets = list(subnet.subnets(new_prefix=25))
+            for sub in subnets:
+                self.handle_subnet(sub, models)
 
     def handle(self, *args, **options):
         logger.info("Starting SNMP discovery process...")
-        asyncio.run(self.process_subnets())
+        self.process_subnets()
 
 def main():
-    call_command('subnet_discovery')
+    Command().handle()
 
 if __name__ == "__main__":
     main()
