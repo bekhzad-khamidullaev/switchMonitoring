3dea1e5667c2b829e0fd5eab5bdbc663750b41e9
Bekhzad Khamidullaev <bekzodhamidullayev74@gmail.com>
2024-04-24 11:35:27 +0500

    ...

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q_c2d9*79bojtl-_la-50e*3b!rqg!gd^@tl@dxe2!dygp6@+%'
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*','ddm.tshtt.uz','10.10.137.120']
 
