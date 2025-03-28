e92c6da923286147d261a7242e3b4b9088187220
Bekhzad Khamidullaev <bekzodhamidullayev74@gmail.com>
2025-03-28 10:44:13 +0500

    sss

--- snmp/views/export.py
+++ snmp/views/export.py
@@ -1,8 +1,15 @@
+import re
 from django.http import HttpResponse
 from openpyxl import Workbook
 from snmp.models import Switch
 from .qoshimcha import get_permitted_branches
 from datetime import datetime
+
+def sanitize_excel_string(value):
+    """Remove invalid characters from strings to prevent Excel errors."""
+    if value:
+        return re.sub(r'[\x00-\x1F\x7F]', '', value)  # Remove control characters
+    return ''
 
 def export_high_sig_switches_to_excel(request):
     current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -14,22 +21,23 @@ def export_high_sig_switches_to_excel(request):
     workbook = Workbook()
     worksheet = workbook.active
     worksheet.append(['Branch', 'ATS', 'Hostname', 'IP', 'Model', 'Uptime', 'RX', 'TX', 'Last check'])
+
     user_permitted_branches = get_permitted_branches(request.user)
     switches = Switch.objects.filter(
         rx_signal__lte=-11, branch__in=user_permitted_branches
     ).order_by('rx_signal')
+
     for switch in switches:
         worksheet.append([
-            switch.ats.branch.name,
-            switch.ats.name,
-            switch.hostname,
-            switch.ip,
-            switch.model.device_model if switch.model else '',
-            switch.uptime or '',
+            sanitize_excel_string(switch.ats.branch.name),
+            sanitize_excel_string(switch.ats.name),
+            sanitize_excel_string(switch.hostname),
+            sanitize_excel_string(switch.ip),
+            sanitize_excel_string(switch.model.device_model) if switch.model else '',
+            sanitize_excel_string(switch.uptime) or '',
             switch.rx_signal,
             switch.tx_signal,
             switch.last_update.strftime('%Y-%m-%d %H:%M:%S') if switch.last_update else '',
-
         ])
 
     workbook.save(response)
