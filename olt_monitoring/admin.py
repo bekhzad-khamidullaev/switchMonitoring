c3e546d1e40a4d958eb33db11ec7dd9621ced882
Bekhzad Khamidullaev <bekzodhamidullayev74@gmail.com>
2025-03-28 13:43:28 +0500

    aaa

--- olt_monitoring/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Olt, Slot
-
-
-admin.site.register(Olt)
-admin.site.register(Slot)
--- olt_monitoring/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from .views.zabbix_sync import sync_hosts_from_zabbix
-
-    
-urlpatterns = [
-    path('synch_zbx/', sync_hosts_from_zabbix, name='sync_zbx'),
-
-]
